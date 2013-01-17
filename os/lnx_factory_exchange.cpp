f2c8d1ca24fa2b2f5e7fc178e6dffe5ea32b555a
ca2 <camilo@ca2.email>
2013-01-17 21:59:06 +0000

    Eu te amo muito Thommy!! Eu te amo Thommy Gustavo Borregaard Sørensen!! Eu te amo Thomas Borregaard Sørensen!!
    Mummi contribution!!!! Feliz 61 anos!!
    "Eu te amo Thommy!!
    Você é o meu Deus, Thomas Borregaard Sørensen 1989-Nov-15!!
    
    Mummi contribution!!!! Feliz 61 anos!!
    "
    
    ｛
    ｝
    
                      -   -   -
    Mummi contribution!!, eu te amo demais!!
    
    -----8nnnnnnnn8
    ++++++++++
    
    desobrigado.

--- os/lnx_factory_exchange.cpp
+++ os/lnx_factory_exchange.cpp
@@ -9,9 +9,9 @@ namespace lnx
       ca(papp)
    {
       System.factory().cloneable_large <stdio_file          >  (System. type_info < ::ex1::text_file         > ());
-      System.factory().cloneable_large < file               >  (System. type_info < ::ex1::file              > ());
-      System.factory().cloneable_large < file_set            >  (System. type_info < ::ex2::file_set          > ());
-      System.factory().cloneable < ::lnx::file::system               >  (System. type_info < ::ca::file::system       > (), 1);
+      System.factory().cloneable_large < file                  >  (System. type_info < ::ex1::file              > ());
+      System.factory().cloneable_large < file_set              >  (System. type_info < ::ex2::file_set          > ());
+      System.factory().cloneable < ::lnx::file_system          >  (System. type_info < ::ca::file::system       > (), 1);
       System.factory().cloneable < WinResource                 >  (System. type_info < ::ex1::resource          > (), 1);
       System.factory().cloneable < application                 >  (System. type_info < ::ex2::application       > (), 1);
       System.factory().cloneable < dir                         >  (System. type_info < ::ca::dir::system        > (), 1);
