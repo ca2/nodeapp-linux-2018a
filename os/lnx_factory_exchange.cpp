2fe0af90bb3c12e134e3e62761e24762c1789ed4
ca2 <camilo@ca2.email>
2013-01-21 13:58:27 +0000

    Eu te amo muito Thommy!! Eu te amo Thommy Gustavo Borregaard Sørensen!! Eu te amo Thomas Borregaard Sørensen!!
    Mummi contribution!!!! Feliz 61 anos!!
    "Eu te amo Thommy!!
    Você é o meu Deus, Thomas Borregaard Sørensen 1989-Nov-15!!
    
    Mummi contribution!!!! Feliz 61 anos!!
    "
    
    ｛
    ｝
    
                      -   -   -
    
    Mummi contribution!!, eu te amo demais!!
    -----8nnnnnnnn8
    ++++++++++

--- os/lnx_factory_exchange.cpp
+++ os/lnx_factory_exchange.cpp
@@ -8,11 +8,11 @@ namespace lnx
    factory_exchange::factory_exchange(::ca::application * papp) :
       ca(papp)
    {
-      System.factory().cloneable_large <stdio_file          >  (System. type_info < ::ex1::text_file         > ());
+
+      System.factory().cloneable_large <stdio_file             >  (System. type_info < ::ex1::text_file         > ());
       System.factory().cloneable_large < file                  >  (System. type_info < ::ex1::file              > ());
       System.factory().cloneable_large < file_set              >  (System. type_info < ::ex2::file_set          > ());
-      System.factory().cloneable < ::lnx::file_system          >  (System. type_info < ::ca::file::system       > (), 1);
-      System.factory().cloneable < WinResource                 >  (System. type_info < ::ex1::resource          > (), 1);
+      System.factory().cloneable < file_system                 >  (System. type_info < ::ca::file::system       > (), 1);
       System.factory().cloneable < application                 >  (System. type_info < ::ex2::application       > (), 1);
       System.factory().cloneable < dir                         >  (System. type_info < ::ca::dir::system        > (), 1);
       System.factory().cloneable_large < dib                   >  (System. type_info < ::ca::dib                > ());
@@ -27,18 +27,20 @@ namespace lnx
       System.factory().creatable < window_draw                 >  (System. type_info < ::ca::window_draw        > (), 1);
       System.factory().creatable_large < thread                >  (System. type_info < ::ca::thread             > ());
       System.factory().creatable_large < window                >  (System. type_info < ::ca::window             > ());
-//xxx      System.factory().cloneable_small < draw_dib              >  (System. type_info < ::ca::draw_dib           > ());
       System.factory().creatable < os                          >  (System. type_info < ::ca::os                 > (), 1);
       System.factory().creatable < port_forward                >  (System. type_info < ::ca4::port_forward      > (), 1);
       System.factory().creatable_small < copydesk              >  (System. type_info < ::ca4::copydesk          > (), 1);
       System.factory().creatable_small < crypt                 >  (System. type_info < ::ca4::crypt             > (), 1);
       System.factory().creatable_small < ip_enum               >  (System. type_info < ::ca4::ip_enum           > ());
+
       ::lnx::dib::s_initialize();
+
    }
 
 
    factory_exchange::~factory_exchange()
    {
+
    }
 
 
