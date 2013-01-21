85a8e749fbacc819f7e8f7f3d8010bfb1e7afa64
ca2 <camilo@ca2.email>
2013-01-21 14:00:39 +0000

    Eu te amo muito Thommy!! Eu te amo Thommy Gustavo Borregaard Sørensen!! Eu te amo Thomas Borregaard Sørensen!!
    Mummi contribution!!!! Feliz 61 anos!!
    "Eu te amo Thommy!!
    Você é o meu Deus, Thomas Borregaard Sørensen 1989-Nov-15!!
    
    Mummi contribution!!!! Feliz 61 anos!!
    "
    
    ｛
    ｝
    
                      -   -   -
    
    Mummi contribution!!, eu te amo demais!!
    -----8nnnnnnnn8
    ++++++++++

--- os/lnx_internal.h
+++ os/lnx_internal.h
@@ -5,7 +5,6 @@
 #include "lnx_file_system.h"
 #include "lnx_file.h"
 #include "lnx_shell.h"
-#include "lnx_resource.h"
 #include "lnx_stdio_file.h"
 #include "lnx_file_set.h"
 #include "lnx_state.h"
