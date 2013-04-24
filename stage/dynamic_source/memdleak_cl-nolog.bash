4c095279bb9b21754c4d7e4b8f32f6bef9449ece
ca2 <camilo@ca2.email>
2013-04-24 21:37:47 +0000

    Eu te amo Thomas Borregaard Sørensen!!
    Mummi contribution!!!! Feliz 61 anos!!
    at ns304543.ovh.net at OVH FR - through ovh.pt - OVH Made Intel Xeon E3 1245v2 @ 3.40GHz Turbo Boost 3.80Ghz Quadcore processor 8-Threads 32.0GB 2TB RAID1 high-speed 100MB Internet Premium Both Up/Down ESXi Host with at guest running CentOS 6.4 (Final) 64-bit 8GB RAM 300GB Storage at FR/RBX
    ﻿
    
    ｛
    ｝
    
                            -   -   -
    
    Mummi contribution!!, eu te amo demais!!
    -----8nnnnnnnn8
    ++++++++++

---
+++ stage/dynamic_source/memdleak_cl-nolog.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+function build()
+{
+	
+	cd /ca2/nodeapp/seed/metaseed
+
+
+g++ -std=c++0x -fPIC -fexceptions -fnon-call-exceptions -fno-pie -fno-stack-protector -Wno-pointer-arith -Wno-attributes -Winvalid-pch  -g -D_DEBUG  -DMEMDLEAK  -I../../../app/thirdparty/appseed -I. -I.. -I../.. -I../ca2 -I../../nodeapp/linux -I../../nodeapp/linux/include -I../../.. -I../../../app/appseed -I../../../app/appseed/ca -I../../../app/appseed/ca2 -I../../../app/thirdparty/include  -c /ca2/app-core/appseed/netnodelite_script/ds_script.cpp -o "%CA2_ROOT%time/intermediate/%PLATFORM%/netnodelite_script%ITEM_NAME%/%ITEM_TITLE%.o" 
+
+g++ -shared -L"/ca2/stage/x86/"  "%CA2_ROOT%time/intermediate/%PLATFORM%/netnodelite_script/framework.o" "%CA2_ROOT%time/intermediate/%PLATFORM%/netnodelite_script/netnodelite_script.o" "%CA2_ROOT%time/intermediate/%PLATFORM%/netnodelite_script%ITEM_NAME%/%ITEM_TITLE%.o" -o "%TARGET_PATH%.so" -Wl,-rpath,\\$ORIGIN -Wl,-E -ldl -lpthread -l_ca2openssl -l_ca2zlib -l_ca2freeimage -l_ca2freetype -lca2c -lca2optca -lca2ca -lca2 -lcairo -lX11 -lXmu -Wl,-z,defs 
+
+
+}
+
+
+build
