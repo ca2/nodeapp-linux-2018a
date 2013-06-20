db6728ab81331e7305740c38d3a6397f37e1f2a8
ca2 <camilo@ca2.email>
2013-06-20 08:08:46 +0000

    Eu te amo Thomas Borregaard Sørensen!!
    Mummi contribution!!!! Feliz 62 anos!!
    
    at ns304543.ovh.net at OVH FR - through ovh.pt - OVH Made Intel Xeon E3 1245v2 @ 3.40GHz Turbo Boost 3.80Ghz Quadcore processor 8-Threads 32.0GB 2TB RAID1 high-speed 100MB Internet Premium Both Up/Down ESXi Host with at guest running CentOS 6.4 (Final) 64-bit 8GB RAM 300GB Storage at FR/RBX
    
    remotely from Curitiba!!
    ﻿
    
    ｛
    ｝
    
                            -   -   -
    
    Mummi contribution!!, eu te amo demais!!
    -----8nnnnnnnn8
    ++++++++++

--- stage/dynamic_source/basis_libl.bash
+++ stage/dynamic_source/basis_libl.bash
@@ -8,7 +8,7 @@ function link()
 
 
 
-g++ -shared -L"%CA2_ROOT%stage/x86/"  %OBJS% -o "%CA2_ROOT%stage/%PLATFORM%/libnetnodelibrary.so" -Wl,-rpath,\\$ORIGIN -Wl,-E -ldl -lpthread -l_ca2openssl -l_ca2zlib -l_ca2freeimage -l_ca2freetype -lca2c -lca2 -lcairo -lX11 -lXmu -lapp_core_netnodelite -Wl,-z,defs &> "%CA2_ROOT%time/dynamic_source/%ITEM_NAME%-link-log.txt"
+g++ -shared -L"%CA2_ROOT%stage/x86/"  %OBJS% -o "%CA2_ROOT%stage/%PLATFORM%/libnetnodelibrary.so" -Wl,-rpath,\\$ORIGIN -Wl,-E -ldl -lpthread -l_ca2openssl -l_ca2zlib -l_ca2freeimage -l_ca2freetype -l_ca2c -lca2ca -lca2 -lcairo -lX11 -lXmu -lapp_core_netnodelite -Wl,-z,defs &> "%CA2_ROOT%time/dynamic_source/%ITEM_NAME%-link-log.txt"
 
 
 }
--- stage/dynamic_source/memdleak_cl-log.bash
+++ stage/dynamic_source/memdleak_cl-log.bash
@@ -9,7 +9,7 @@ function build()
 
 g++ -std=c++0x -fPIC -fexceptions -fnon-call-exceptions -fno-pie -fno-stack-protector -Wno-pointer-arith -Wno-attributes -Winvalid-pch  -g -D_DEBUG  -DMEMDLEAK  -I../../../app/thirdparty/appseed -I. -I.. -I../.. -I../ca2 -I../../nodeapp/linux -I../../nodeapp/linux/include -I../../.. -I../../../app/appseed -I../../../app/appseed/ca -I../../../app/appseed/ca2 -I../../../app/thirdparty/include  -c /ca2/app-core/appseed/netnodelite_script/ds_script.cpp -o "%CA2_ROOT%time/intermediate/%PLATFORM%/netnodelite_script%ITEM_NAME%/%ITEM_TITLE%.o" &> "%CA2_ROOT%time/dynamic_source%ITEM_NAME%-compile-log.txt"
 
-g++ -shared -L"/ca2/stage/x86/"  "%CA2_ROOT%time/intermediate/%PLATFORM%/netnodelite_script/framework.o" "%CA2_ROOT%time/intermediate/%PLATFORM%/netnodelite_script/netnodelite_script.o" "%CA2_ROOT%time/intermediate/%PLATFORM%/netnodelite_script%ITEM_NAME%/%ITEM_TITLE%.o" -o "%TARGET_PATH%.so" -Wl,-rpath,\\$ORIGIN -Wl,-E -ldl -lpthread -l_ca2openssl -l_ca2zlib -l_ca2freeimage -l_ca2freetype -lca2c -lca2optca -lca2ca -lca2 -lcairo -lX11 -lXmu -Wl,-z,defs &> "%CA2_ROOT%time/dynamic_source%ITEM_NAME%-link-log.txt"
+g++ -shared -L"/ca2/stage/x86/"  "%CA2_ROOT%time/intermediate/%PLATFORM%/netnodelite_script/framework.o" "%CA2_ROOT%time/intermediate/%PLATFORM%/netnodelite_script/netnodelite_script.o" "%CA2_ROOT%time/intermediate/%PLATFORM%/netnodelite_script%ITEM_NAME%/%ITEM_TITLE%.o" -o "%TARGET_PATH%.so" -Wl,-rpath,\\$ORIGIN -Wl,-E -ldl -lpthread -l_ca2openssl -l_ca2zlib -l_ca2freeimage -l_ca2freetype -l_ca2c -lca2ca -lca2 -lcairo -lX11 -lXmu -Wl,-z,defs &> "%CA2_ROOT%time/dynamic_source%ITEM_NAME%-link-log.txt"
 
 
 }
