46e1d81e6a9d9879dd5865707efc894d60330e54
ca2 <camilo@ca2.email>
2014-09-05 19:39:48 +0000

    from inha ubuntu after a long time without working in ubuntu since started working deeply with GTECH/sensible/Projeto Açúcar and lately with many deep  (AND VERY EFFECTIVE, thanks to God and Mummi!!) imporvements to CA2 (ca2 Desenvolvimento de Software)!! inha ubuntu as VMware guest... now with much more practice and experience in correcting common errors in Linux environment during project compilation and linking and also setting up environment.!! And now fixing deeply and far to perfectness!! now at Curitiba, My God and My Goddess both of the same City [Curitiba] (Mummi and Thomas, as three Phonexies with me)
    <3JegElskerDigThommy!!
    Mummi contribution!!!!
    
    ｛
    ｝
    
       ----
                            -   -   -
    Mummi contribution!!, eu te amo demais!!
    -----8nnnnnnnn8
    ++++++++++
    from inha ubuntu after a long time without working in ubuntu since started working deeply with GTECH/sensible/Projeto Açúcar and lately with many deep  (AND VERY EFFECTIVE, thanks to God and Mummi!!) imporvements to CA2 (ca2 Desenvolvimento de Software)!! inha ubuntu as VMware guest... now with much more practice and experience in correcting common errors in Linux environment during project compilation and linking and also setting up environment.!! And now fixing deeply and far to perfectness!! now at Curitiba, My God and My Goddess both of the same City [Curitiba] (Mummi and Thomas, as three Phonexies with me)
    
    <3JegElskerDigThommy!!
    Mummi contribution!!!!
    
    ｛
    ｝
    
       ----
                            -   -   -
    Mummi contribution!!, eu te amo demais!!
    -----8nnnnnnnn8
    ++++++++++
    from inha ubuntu after a long time without working in ubuntu since started working deeply with GTECH/sensible/Projeto Açúcar and lately with many deep  (AND VERY EFFECTIVE, thanks to God and Mummi!!) imporvements to CA2 (ca2 Desenvolvimento de Software)!! inha ubuntu as VMware guest... now with much more practice and experience in correcting common errors in Linux environment during project compilation and linking and also setting up environment.!! And now fixing deeply and far to perfectness!! now at Curitiba, My God and My Goddess both of the same City [Curitiba] (Mummi and Thomas, as three Phonexies with me)
    
    <3JegElskerDigThommy!!
    Mummi contribution!!!!
    
    ｛
    ｝
    
       ----
                            -   -   -
    Mummi contribution!!, eu te amo demais!!
    -----8nnnnnnnn8
    ++++++++++
    from inha ubuntu after a long time without working in ubuntu since started working deeply with GTECH/sensible/Projeto Açúcar and lately with many deep  (AND VERY EFFECTIVE, thanks to God and Mummi!!) imporvements to CA2 (ca2 Desenvolvimento de Software)!! inha ubuntu as VMware guest... now with much more practice and experience in correcting common errors in Linux environment during project compilation and linking and also setting up environment.!! And now fixing deeply and far to perfectness!! now at Curitiba, My God and My Goddess both of the same City [Curitiba] (Mummi and Thomas, as three Phonexies with me)
    
    <3JegElskerDigThommy!!
    Mummi contribution!!!!
    
    ｛
    ｝
    
       ----
                            -   -   -
    Mummi contribution!!, eu te amo demais!!
    -----8nnnnnnnn8
    ++++++++++

--- operational_system/_.h
+++ operational_system/_.h
@@ -15,8 +15,12 @@
 
 #define _ISOC99_SOURCE
 #define _ISOC11_SOURCE
-#define _POSIX_SOURCE
+#ifndef _POSIX_SOURCE
+#define _POSIX_SOURCE 1
+#endif // _POSIX_SOURCE
+#ifndef _POSIX_C_SOURCE
 #define _POSIX_C_SOURCE 200809L
+#endif // _POSIX_C_SOURCE
 #define _XOPEN_SOURCE 700
 #define _XOPEN_SOURCE_EXTENDED
 #define _LARGEFILE_SOURCE
