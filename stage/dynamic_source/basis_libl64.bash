574071f6becc18835ecb5a61cddc5d857c590083
ca2 <camilo@ca2.email>
2015-02-16 02:14:41 +0000

    <3JegElskerDigThommy!!

--- stage/dynamic_source/basis_libl64.bash
+++ stage/dynamic_source/basis_libl64.bash
@@ -8,7 +8,7 @@ function link()
 
 
 
-g++ -shared -L"%CA2_ROOT%stage/x86/"  %OBJS% -o "%CA2_ROOT%stage/%PLATFORM%/libnetnodelibrary.so" -Wl,-rpath,\\$ORIGIN -Wl,-E -ldl -lpthread -laura -laxiscrypto -laxisopenssl -laxiszlib -laxisfreeimage -laxisfreetype -laxispixman -laxiscairo -lbase -lcore -lX11 -lXmu -lnetnode -Wl,-z,defs &> "%CA2_ROOT%time/dynamic_source/%ITEM_NAME%-link-log.txt"
+g++ -shared -L"%CA2_ROOT%stage/x86/"  %OBJS% -o "%CA2_ROOT%stage/x86/libnetnodelibrary.so" -Wl,-rpath,\\$ORIGIN -Wl,-E -ldl -lpthread -laura -laxiscrypto -laxisopenssl -laxiszlib -laxisfreeimage -laxisfreetype -laxispixman -laxiscairo -laxis -lbase -lcore -lprogramming -lX11 -lXmu -lnetnode -Wl,-z,defs &> "%CA2_ROOT%time/dynamic_source/%ITEM_NAME%-link-log.txt"
 
 
 }
