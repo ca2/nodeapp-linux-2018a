320f5c935f81f77c2f49d263987b3d7b5b82a01f
ca2 <camilo@ca2.email>
2016-03-27 10:48:15 +0000

    Mummi contribution!! LCTV contribution!! catherinacortega contribution!! fuel contribution!! pablosj contribution!! eax contribution!! magrib contribution!!
    from bilbo-ubuntu at veriverse!!
    at /xcore/ folder

---
+++ stage/introjection/basis_l64.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+function link()
+{
+	
+	cd %CA2_ROOT%nodeapp/seed/metaseed
+
+
+
+g++ -shared -L"%CA2_ROOT%stage/x86/"  "%CA2_ROOT%time/intermediate/%PLATFORM%/%CONFIGURATION_NAME%/xmpp_resident/%ITEM_NAME%.o" -o "%TARGET_PATH%.so" -Wl,-rpath,\\$ORIGIN -Wl,-E -ldl -lpthread -laura -laxiscrypto -laxisopenssl -laxiszlib -laxisfreeimage -laxisfreetype -laxispixman -laxiscairo -laxis -lbase -lcore -lprogramming -lX11 -lXmu -lprogramming -lvericard -lirc -lxmpp -lapp_core_irc_bot -lapp_core_irc_client -lapp_core_irc_resident -lapp_core_xmpp_bot -lapp_core_xmpp_client -lapp_core_xmpp_resident -lapp_core_resident -Wl,-z,defs 
+
+
+}
+
+
+link
