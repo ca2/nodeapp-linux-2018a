402ac0c236f165ce90e07d1b9d3ac05a3e888bef
ca2 <camilo@ca2.email>
2016-07-28 22:13:50 +0000

    bilbo contribution!! Mummi contribution!! veriverse contribution!! Catherina Cortega, Benjamin Kuhl,  Pawel Kulesza, chubz, diagram, hudwan, intelijens, theaifam5 (Mateusz Paluszkiewicz), bigboom, dimitris1989, dopedopedope, dzetko, pethervini, polandball, shanshanren, thunderwill, kaeris, kamok,  kissloryshy, kord and kranalex contribution!! Twtich contribution!! Connavardev, Gaseline, Kurroqt, Landavaranda and Odielag contribution!! from bilbo Ubuntu!! i7 6700 32GB RAM

--- stage/introjection/basis_l64.bash
+++ stage/introjection/basis_l64.bash
@@ -8,7 +8,7 @@ function link()
 
 
 
-g++ -shared -L"%CA2_ROOT%stage/x86/"  "%ITEM_NAME%.o" -o "%TARGET_PATH%.so" -Wl,-rpath,\\$ORIGIN -Wl,-E -ldl -lpthread -laura -laxiscrypto -laxisopenssl -laxiszlib -laxisfreeimage -laxisfreetype -laxispixman -laxiscairo -laxis -lbase -lcore -lprogramming -lX11 -lXmu -lprogramming -lvericard -lbeampro -lirc -lxmpp -lapp_core_beampro_bot -lapp_core_beampro_client -lapp_core_beampro_resident -lapp_core_irc_bot -lapp_core_irc_client -lapp_core_irc_resident -lapp_core_xmpp_bot -lapp_core_xmpp_client -lapp_core_xmpp_resident -lapp_core_resident -Wl,-z,defs 
+g++ -shared -L"%CA2_ROOT%stage/x86/"  "%ITEM_NAME%.o" -o "%TARGET_PATH%.so" -Wl,-rpath,\\$ORIGIN -Wl,-E -ldl -lpthread -laura -lcrypto -lssl -lz -laxisfreeimage -lfreetype -lcairo -laxis -lbase -lcore -lprogramming -lX11 -lXmu -lprogramming -lvericard -lbeampro -lirc -lxmpp -lapp_core_beampro_bot -lapp_core_beampro_client -lapp_core_beampro_resident -lapp_core_irc_bot -lapp_core_irc_client -lapp_core_irc_resident -lapp_core_xmpp_bot -lapp_core_xmpp_client -lapp_core_xmpp_resident -lapp_core_resident -Wl,-z,defs 
 
 
 }
