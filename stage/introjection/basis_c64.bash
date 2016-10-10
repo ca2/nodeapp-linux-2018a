c3e6ced82a976721b2853ccdcd4e881d63abac1a
ca2 <camilo@ca2.email>
2016-10-10 09:41:32 +0000

    bilbo and Mummi contribution!! veriverse contribution!! LCTV contribution!! Catherina Cortega, Pawel Kulesza (sjpablo, pablosj), doraemon (gosu), drmjg, hudwan, kissloryshy, madder, marianyc, nuke, systemis and v0n3x contribution!! Twitch contribution!! AlfredoPlays (and his mates), GrubbyJuice and Mattyplays6 contribution!! Beam contribution!!  bisba contribution!! caro contribution!!
    bilbo at veriverse
    i7 6700 Ubuntu 16.04.1

--- stage/introjection/basis_c64.bash
+++ stage/introjection/basis_c64.bash
@@ -7,7 +7,7 @@ function compile()
 	cd %CA2_ROOT%nodeapp/seed/metaseed
 
 
-g++ -std=c++0x -fexceptions -fnon-call-exceptions -fno-pie -fno-stack-protector -Wno-pointer-arith -Wno-attributes -Winvalid-pch -fPIC -std=c++14  -g -D_DEBUG    -I../../../include -I../../../app-core/appseed/netnode_dynamic_source_library -I../../../app/thirdparty/appseed -I. -I.. -I../.. -I../core -I../../nodeapp/linux -I../../nodeapp/linux/include -I../../../app -I../../../app/include -I../../.. -I../../../app/appseed -I../../../app/appseed/core -I../../../app/appseed/base -I../../../app/appseed/axis -I../../../app/appseed/aura -I../../../app/axis  -I/netnodenet/net/netseed/include -c "%SOURCE%" -o "%ITEM_NAME%.o"
+g++ -std=c++0x -fexceptions -fnon-call-exceptions -fno-pie -fno-stack-protector -Wno-pointer-arith -Wno-attributes -Winvalid-pch -fPIC -std=c++14  -g -D_DEBUG   -I/xcore/app-core/appseed/resident -I../../../include -I../../../app/thirdparty/appseed -I. -I.. -I../.. -I../core -I../../nodeapp/linux -I../../nodeapp/linux/include -I../../../app -I../../../app/include -I../../.. -I../../../app/appseed -I../../../app/appseed/core -I../../../app/appseed/base -I../../../app/appseed/axis -I../../../app/appseed/aura -I../../../app/axis   -c "%SOURCE%" -o "%ITEM_NAME%.o"
 
 
 }
